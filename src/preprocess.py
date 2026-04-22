e60b3f1fa8eb71acbf42db833ad4e1a9281fbb5d
Shikha Singh <shikha.singh09jan@gmail.com>
2026-04-22 15:58:37 +0530

    data-clean: added stopwords removal

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,15 @@
 import re
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
 
 def clean_text(text):
     text = text.lower()
-    text = re.sub(r"http\S+", "", text)      # remove links
-    text = re.sub(r"[^a-zA-Z]", " ", text)   # remove special characters
-    return text.strip()
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"[^a-zA-Z]", " ", text)
+
+    words = text.split()
+    words = [w for w in words if w not in stopwords.words('english')]
+
+    return " ".join(words)
