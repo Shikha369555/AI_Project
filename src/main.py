f9520098e95c0fbaad2de5f050765622fe9f8b11
Shikha Singh <shikha.singh09jan@gmail.com>
2026-04-23 17:33:10 +0530

    feature: implemented and verified train-test split

--- src/main.py
+++ src/main.py
@@ -41,3 +41,5 @@ print(y.unique())
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+print("\nTrain shape:", X_train.shape)
+print("Test shape:", X_test.shape)
