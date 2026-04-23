a729a84f7820f0c50947a8419641e7dbf84f9acb
Shikha Singh <shikha.singh09jan@gmail.com>
2026-04-23 17:37:46 +0530

    model: trained Logistic Regression on training data

--- src/main.py
+++ src/main.py
@@ -44,3 +44,4 @@ X_train, X_test, y_train, y_test = train_test_split(
 print("\nTrain shape:", X_train.shape)
 print("Test shape:", X_test.shape)
 model = LogisticRegression()
+model.fit(X_train, y_train)
