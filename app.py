46de6b8f324dcc795f69e302a5e48510428e6dab
Shikha Singh <shikha.singh09jan@gmail.com>
2026-04-24 12:43:16 +0530

    ui: improved input field with placeholder

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="AI Grievance System", page_icon="🤖", layout="centered")
 st.markdown("<h1 style='text-align: center; color: #4CAF50;'>🤖 AI Grievance System</h1>", unsafe_allow_html=True)
 st.markdown(
-    "<p style='text-align: center;color: 	#228B22'>Enter your complaint and get the predicted department instantly.</p>",
+    "<p style='text-align: center;'>Enter your complaint and get the predicted department instantly.</p>",
     unsafe_allow_html=True
 )
+user_input = st.text_input("📝 Your Complaint", placeholder="e.g., Water leakage in my area")
