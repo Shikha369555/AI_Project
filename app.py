220ecc2e6680244557602f708f1177d6daa247aa
Shikha Singh <shikha.singh09jan@gmail.com>
2026-04-24 12:40:12 +0530

    ui: added styled description

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title="AI Grievance System", page_icon="🤖", layout="centered")
 st.markdown("<h1 style='text-align: center; color: #4CAF50;'>🤖 AI Grievance System</h1>", unsafe_allow_html=True)
+st.markdown(
+    "<p style='text-align: center;color: 	#228B22'>Enter your complaint and get the predicted department instantly.</p>",
+    unsafe_allow_html=True
+)
